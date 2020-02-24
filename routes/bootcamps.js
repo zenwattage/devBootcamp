e85b5135873e1daa4937dd80925ceb88403cfaf3
Scott H <zenwattage@gmail.com>
2020-02-24 10:35:15 -0800

    routes mounted and exported

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,27 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/api/v1/bootcamps', (req, res) => {
+router.get('/, (req, res) => {
     // res.json({ name: 'Scott' });
     res.status(200).json({ success: true, msg: 'Show all bootcamps' });
 });
 
-router.get('/api/v1/bootcamps/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     // res.json({ name: 'Scott' });
     res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
 });
 
-router.post('/api/v1/bootcamps', (req, res) => {
+router.post('/, (req, res) => {
     // res.json({ name: 'Scott' });
     res.status(200).json({ success: true, msg: 'Create new bootcamp' });
 });
 
-router.put('/api/v1/bootcamps/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     // res.json({ name: 'Scott' });
     res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 });
 
-router.delete('/api/v1/bootcamps/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     // res.json({ name: 'Scott' });
     res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 });
+
+module.exports = router;
