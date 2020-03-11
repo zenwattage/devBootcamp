c8f5293e051cdad5358a05173392dcc631e3b554
Scott H <zenwattage@gmail.com>
2020-03-11 00:56:19 -0700

    adding slugify, geocoder, and mapquest api functionality

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const slugify = require('slugify');
-// const geocoder = require('../utils/geocoder');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema(
     {
@@ -41,6 +41,7 @@ const BootcampSchema = new mongoose.Schema(
         },
         location: {
             // GeoJSON Point
+
             type: {
                 type: String,
                 enum: ['Point']
@@ -112,30 +113,36 @@ const BootcampSchema = new mongoose.Schema(
         // }
     });
 
-// // Create bootcamp slug from the name
-// BootcampSchema.pre('save', function (next) {
-//     this.slug = slugify(this.name, { lower: true });
-//     next();
-// });
 
-// // Geocode & create location field
-// BootcampSchema.pre('save', async function (next) {
-//     const loc = await geocoder.geocode(this.address);
-//     this.location = {
-//         type: 'Point',
-//         coordinates: [loc[0].longitude, loc[0].latitude],
-//         formattedAddress: loc[0].formattedAddress,
-//         street: loc[0].streetName,
-//         city: loc[0].city,
-//         state: loc[0].stateCode,
-//         zipcode: loc[0].zipcode,
-//         country: loc[0].countryCode
-//     };
 
-//     // Do not save address in DB
-//     this.address = undefined;
-//     next();
-// });
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+    console.log('slugify ran', this.name);
+
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function (next) {
+
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode
+    };
+
+    // Do not save address in DB
+    this.address = undefined;
+    next();
+});
 
 // // Cascade delete courses when a bootcamp is deleted
 // BootcampSchema.pre('remove', async function (next) {
