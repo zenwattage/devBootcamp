6cfb9a50d67042484d6b13d0dbde7cc98fdff78c
Scott H <zenwattage@gmail.com>
2020-03-10 18:48:49 -0700

    initial push of routes, controllers, custom error handlers, and middleware

---
+++ controllers/bootcamps.js
@@ -0,0 +1,98 @@
+const ErrorResponse = require('../utils/errorResponse');
+const Bootcamp = require('../models/Bootcamp');
+
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+// @desc    Get single bootcamp
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        // console.log(req.body);
+        if (!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        }
+
+        res.status(200).json({ success: true, data: bootcamp });
+
+    } catch (err) {
+        // res.status(400).json({ success: false, message: `Bootcamp not found with id of ${req.params.id}` });
+        next(err);
+    }
+};
+
+// @desc    Create a new bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = async (req, res, next) => {
+
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        next(err);
+    }
+
+
+};
+
+// @desc    Update bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = async (req, res, next) => {
+
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        }
+
+
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        next(err);
+    }
+
+};
+
+// @desc    delete bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = async (req, res, next) => {
+
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        if (!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        }
+
+
+        res.status(200).json({ success: true, data: {} });
+    } catch (err) {
+        next(err);
+    }
+
+};
--- server.js
+++ server.js
@@ -1,22 +1,50 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-//route files
-const bootcamps = require('./routes/bootcamps');
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 
 //load env config vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+//route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// customer logger middlerware
+// app.use(logger);
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 //mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
+    console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server and exit process
+    server.close(() => process.exit(1));
+});
